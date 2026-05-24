d2af6ec8ff2e4836d9f1e540ee8efc63413186ce
Yelyzaveta Vecherovska <vecheli0306@gmail.com>
2026-05-24 20:29:19 +0300

    add error handlers and logger

--- src/server.js
+++ src/server.js
@@ -1,16 +1,25 @@
 import express from 'express';
 import 'dotenv/config';
 import cors from 'cors';
+import pino from 'pino-http';
 
-import { connectMongoDB } from './db/connectMongoDB.js';
+// import { connectMongoDB } from './db/connectMongoDB.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3030;
 
 app.use(express.json());
 app.use(cors());
+app.use(pino());
 
-await connectMongoDB();
+// await connectMongoDB();
+
+app.use(notFoundHandler);
+
+app.use(errorHandler);
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
